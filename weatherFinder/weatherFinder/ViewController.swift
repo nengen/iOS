60dc9d28d5a19b2752002bfb7cab4a2ceadc7e60
nengen <engennilserik@gmail.com>
2018-09-26 11:39:52 +0200

    Added autocomplete, need to change it so I get a table view with suggestions

--- weatherFinder/weatherFinder/ViewController.swift
+++ weatherFinder/weatherFinder/ViewController.swift
@@ -8,16 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate{
+class ViewController: UIViewController, UITextFieldDelegate{
     
     @IBOutlet weak var cityTextField: UITextField!
     @IBAction func showWeatherButton(_ sender: UIButton) {
     }
     @IBOutlet weak var showWeatherOutlet: UIButton!
     @IBOutlet weak var answerLabel: UILabel!
-    let autoCompleteTableView = UITableView.init(frame: CGRect(x:0,y:80,width: 320,height:120), style: UITableViewStyle.plain)
-    var pastUrls = ["Men", "Women", "Cats", "Dogs", "Children"]
-    var autocompleteUrls = [String]()
+
+    var autoCompletePossibilities = ["Bergen,DE", "Bergen,NO", "Cats", "Dogs", "Children"]
+    var autocompleteCharactersCount = 0
+    var timer = Timer()
     
     
     
@@ -46,67 +47,27 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cityTextField.layer.cornerRadius = 6
         cityTextField.placeholder = "Enter value"
         
-        
-        
-        //autoComplete
-        autoCompleteTableView.delegate = self
-        autoCompleteTableView.dataSource = self
-        autoCompleteTableView.isScrollEnabled = true
-        autoCompleteTableView.isHidden = true
-        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    func searchAutocompleteEntriesWithSubstring(substring: String)
-    {
-        autocompleteUrls.removeAll(keepingCapacity: false)
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        var subString = (cityTextField.text!.capitalized as NSString).replacingCharacters(in: range, with: string)
+        subString = formatSubstring(subString: subString)
         
-        for curString in pastUrls
-        {
-            let myString:NSString! = curString as NSString
-            
-            let substringRange :NSRange! = myString.range(of: substring)
-            
-            if (substringRange.location  == 0)
-            {
-                autocompleteUrls.append(curString)
-            }
+        if subString.count == 0{
+            resetValues()
+        }else{
+            searchAutoCompleteEntriesWithSubstring(substring: subString)
         }
+        return true
         
-        autoCompleteTableView.reloadData()
     }
     
-    func textField(textField: UITextField!, shouldChangeCharactersInRange range: NSRange, replacementString string: String!) -> Bool
-    {
-        autoCompleteTableView.isHidden = false
-        let substring = (textField.text! as NSString).replacingCharacters(in: range, with: string)
-        
-        searchAutocompleteEntriesWithSubstring(substring: substring)
-        return true     // not sure about this - could be false
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return autocompleteUrls.count
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
-        let autoCompleteRowIdentifier = "AutoCompleteRowIdentifier"
-        let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: autoCompleteRowIdentifier, for: indexPath as IndexPath) as UITableViewCell
-        let index = indexPath.row as Int
-        
-        cell.textLabel!.text = autocompleteUrls[index]
-        return cell
-    }
-    
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let selectedCell : UITableViewCell = tableView.cellForRow(at: indexPath as IndexPath)!
-        textField.text = selectedCell.textLabel.text
-    }
-    
+   
     
     
 }
--- weatherFinder/weatherFinder/autoComplete.swift
+++ weatherFinder/weatherFinder/autoComplete.swift
@@ -11,28 +11,66 @@ import UIKit
 import CoreGraphics
 
 extension ViewController{
-    func createAutoCompleteTableView()->UITableView{
-        let autoCompleteTableView = UITableView.init(frame: CGRect(x:0,y:80,width: 320,height:120), style: UITableViewStyle.plain)
-        autoCompleteTableView.delegate = (cityTextField as! UITableViewDelegate)
-        autoCompleteTableView.dataSource = (cityTextField as! UITableViewDataSource)
-        autoCompleteTableView.isScrollEnabled = true
-        autoCompleteTableView.isHidden = true
-        self.view.addSubview(autoCompleteTableView)
-        return autoCompleteTableView
+    func formatSubstring(subString: String) -> String{
+        let formatted = String(subString.dropLast(autocompleteCharactersCount)).lowercased().capitalized
+        return formatted
     }
     
-    func startAutoComplete(tableView: UITableView){
-        if cityTextField.text?.count ?? 0 > 2 {
-            tableView.isHidden = false
-        }
+    func resetValues(){
+        autocompleteCharactersCount = 0
+        cityTextField.text = ""
     }
     
-    func getSubstring(tableView: UITableView)->String{
-        if tableView.isHidden == false {
-            return cityTextField.text!
+    func searchAutoCompleteEntriesWithSubstring(substring: String){
+        let userQuery = substring
+        let suggestions = getAutoCompleteSuggestions(userText: substring)
+        
+        if suggestions.count > 0 {
+            timer = .scheduledTimer(withTimeInterval: 0.01, repeats: false, block: {(timer) in
+                let autoCompleteResult = self.formatAutoCompleteResult(substring: substring, possibleMatches: suggestions)
+                self.putColourFormattedTextInTextField(autoCompleteResult: autoCompleteResult, userQuery: userQuery)
+                self.moveCaretToEndUserQueryPosition(userQuery: userQuery)
+            })
         }else{
-            return "None"
+            timer = .scheduledTimer(withTimeInterval: 0.01, repeats: false, block: {(timer) in
+                self.cityTextField.text = substring
+            })
+            autocompleteCharactersCount = 0
         }
+    }
+    
+    func getAutoCompleteSuggestions(userText: String) -> [String]{
+        var possibleMatches: [String] = []
+        for item in autoCompletePossibilities {
+            let myString:NSString! = item as NSString
+            let substringRange: NSRange! = myString.range(of: userText)
+            
+            if(substringRange.location == 0){
+                possibleMatches.append(item)
+            }
+        }
+        return possibleMatches
+    }
+    
+    func putColourFormattedTextInTextField(autoCompleteResult: String, userQuery: String){
+        let colouredString: NSMutableAttributedString = NSMutableAttributedString(string: userQuery + autoCompleteResult)
+        colouredString.addAttribute(NSAttributedStringKey.foregroundColor, value: UIColor.lightGray, range: NSRange(location: userQuery.count, length: autoCompleteResult.count))
+        self.cityTextField.attributedText = colouredString
+    }
+    
+    func moveCaretToEndUserQueryPosition(userQuery: String){
+        if let newPosition = self.cityTextField.position(from: self.cityTextField.beginningOfDocument, offset: userQuery.count){
+            self.cityTextField.selectedTextRange = self.cityTextField.textRange(from: newPosition, to: newPosition)
+        }
+        let selectedRange: UITextRange? = cityTextField.selectedTextRange
+        cityTextField.offset(from: cityTextField.beginningOfDocument, to: (selectedRange?.start)!)
+    }
+    
+    func formatAutoCompleteResult(substring: String, possibleMatches: [String]) -> String {
+        var autoCompleteResult = possibleMatches[0]
+        autoCompleteResult.removeSubrange(autoCompleteResult.startIndex..<autoCompleteResult.index(autoCompleteResult.startIndex, offsetBy: substring.count))
+        autocompleteCharactersCount = autoCompleteResult.count
+        return autoCompleteResult
     }
     
     
